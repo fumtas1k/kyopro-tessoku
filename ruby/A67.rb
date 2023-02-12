98fbf8d73f592483911df5836e41777e251e3c31
fumtas1k <fumta01a7@gmail.com>
2023-02-12 22:16:51 +0900

    [add]A67 ruby 追加

---
+++ ruby/A67.rb
@@ -0,0 +1,49 @@
+# A67
+# 最小全域木
+# クラスカル法(貪欲法 + Union-Find木)
+# 実行時間: 1s以内
+
+start_time = Time.now
+
+class UnionFind
+  attr_accessor :parents
+
+  def initialize(size)
+    @parents = Array.new(size, &:itself)
+  end
+
+  def root(u)
+    return u if u == parents[u]
+    parents[u] = root(parents[u])
+  end
+
+  def unite(u, v)
+    ru, rv = root(u), root(v)
+    return if ru == rv
+    parents[rv] = ru
+  end
+
+  def same?(u, v)
+    root(u) == root(v)
+  end
+end
+
+File.open("question/#{File.basename(__FILE__).split(/\.rb$/).first}.txt", "r") do |f|
+  N, M = f.gets.split.map(&:to_i)
+  ABC = Array.new(M) { f.gets.split.map(&:to_i) }.sort_by { _1[-1] }
+end
+
+uf = UnionFind.new(N + 1)
+
+ans = ABC.sum do |a, b, c|
+  if uf.same?(a, b)
+    0
+  else
+    uf.unite(a, b)
+    c
+  end
+end
+
+puts ans
+
+puts "\n処理時間: #{((Time.now - start_time) * 1_000).round(2)} ms"
