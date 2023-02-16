4c1e80ea88880a2193845b268c0251610470ee0e
fumtas1k <fumta01a7@gmail.com>
2023-02-17 00:06:17 +0900

    [add]gradle追加

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+  kotlin("jvm") version "1.8.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  testImplementation(kotlin("test"))
+  implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+
+}
+
+tasks.test {
+  useJUnitPlatform()
+}
+
+kotlin {
+  jvmToolchain(11)
+}
