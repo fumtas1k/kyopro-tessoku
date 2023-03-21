d78c5538ff9000faeaa34e3d3507a659bf1198d2
fumtas1k <fumta01a7@gmail.com>
2023-03-21 18:13:19 +0900

    [change]シンプルに記述

--- ruby/A69.rb
+++ ruby/A69.rb
@@ -7,15 +7,15 @@ require_relative "lib/max_flow"
 File.open("question/#{File.basename(__FILE__).split(/\.rb$/).first}.txt", "r") do |f|
   N = f.gets.to_i
   C = Array.new(N) { f.gets.chomp.chars }
-  max_flow = MaxFlow.new(2 * N + 2)
-  C.each_with_index do |row, i|
-    row.each_with_index do |c, j|
-      max_flow.add_edge(i, N + j, 1)
-    end
-  end
-  1.upto(N) do |i|
-    max_flow.add_edge(2 * N + 1, i, 1)
-    max_flow.add_edge(N + i, 2 * N + 2, 1)
-  end
-  puts max_flow.max_flow(2 * N + 1, 2 * N + 2)
 end
+
+max_flow = MaxFlow.new(2 * N + 2)
+1.upto(N) do |i|
+  max_flow.add_edge(2 * N + 1, i, 1)
+  max_flow.add_edge(N + i, 2 * N + 2, 1)
+  1.upto(N) do |j|
+    next if C[i - 1][j - 1] == "."
+    max_flow.add_edge(i, N + j, 1)
+  end
+end
+puts max_flow.max_flow(2 * N + 1, 2 * N + 2)
