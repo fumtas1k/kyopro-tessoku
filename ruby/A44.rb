778c4bd1b9d8ebaf0b120f10a8c3645f09a9e0fd
fumtas1k <fumta01a7@gmail.com>
2023-05-14 14:01:17 +0900

    [fix]変数名をわかりやすいものに修正

--- ruby/A44.rb
+++ ruby/A44.rb
@@ -6,18 +6,18 @@ N, Q = gets.split.map(&:to_i)
 QUERY = Array.new(Q) { gets.split.map(&:to_i) }
 
 A = [*0 .. N]
-rev = false
+is_reverse = false
 QUERY.each do |q|
   case q[0]
   when 1
-    if rev
+    if is_reverse
       A[N - q[1] + 1] = q[2]
     else
       A[q[1]] = q[2]
     end
   when 2
-    rev = !rev
+    is_reverse = !is_reverse
   when 3
-    puts rev ? A[N - q[1] + 1] : A[q[1]]
+    puts is_reverse ? A[N - q[1] + 1] : A[q[1]]
   end
 end
