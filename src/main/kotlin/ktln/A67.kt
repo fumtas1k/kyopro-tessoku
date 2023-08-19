090e6208122714ba26fd29212cad0c080afd78d7
fumtas1k <fumta01a7@gmail.com>
2023-08-19 11:05:38 +0900

    [add]A67 kotllin追加

---
+++ src/main/kotlin/ktln/A67.kt
@@ -0,0 +1,45 @@
+/**
+ * A67
+ * 最小全域木
+ * クラスカル法(貪欲法 + Union-Find木)
+ * 実行時間: 1s以内
+ */
+
+package ktln
+
+class UnionFind(private val size: Int) {
+  private val parents: IntArray
+  init {
+    parents = IntArray(size + 1) { it }
+  }
+
+  fun root(u: Int): Int {
+    if (u == parents[u]) return u
+    parents[u] = root(parents[u])
+    return parents[u]
+  }
+
+  fun unite(u: Int, v: Int) {
+    val ru = root(u)
+    val rv = root(v)
+    if (ru == rv) return
+    parents[rv] = ru
+  }
+
+  fun isSame(u: Int, v: Int): Boolean = root(u) == root(v)
+}
+
+fun main() {
+  val (N, M) = readLine()!!.split(" ").map(String::toInt)
+  val ABC = Array(M) { readLine()!!.split(" ").map(String::toInt) }.sortedWith(compareBy({it.last()}))
+
+  var ans = 0
+  val uf = UnionFind(N + 1)
+  ABC.forEach { (a, b, c) ->
+    if (uf.isSame(a, b)) return@forEach
+    uf.unite(a, b)
+    ans += c
+  }
+
+  println(ans)
+}
