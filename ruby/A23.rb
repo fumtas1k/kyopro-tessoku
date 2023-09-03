01bd98144b30c01377e2aeeaccad4b95587b47e1
fumtas1k <fumta01a7@gmail.com>
2023-09-03 20:57:04 +0900

    [improve]不要なカラム削除

--- ruby/A23.rb
+++ ruby/A23.rb
@@ -5,14 +5,14 @@
 
 N, M = gets.split.map(&:to_i)
 A = Array.new(M) { gets.split.join.to_i(2) }
-dp = Array.new(M + 1) { [Float::INFINITY] * (1 << N) }
-dp[0][0] = 0
-1.upto(M) do |i|
-  a = A[i - 1]
-  ((1 << N) - 1).downto(0) do |j|
-    dp[i][j] = dp[i - 1][j]
-    dp[i][j | a] = [dp[i][j | a], dp[i - 1][j] + 1].min
+
+dp = Array.new(1 << N, Float::INFINITY)
+dp[0] = 0
+
+(1 << N).times do |bits|
+  M.times do |i|
+    dp[bits | A[i]] = [dp[bits | A[i]], dp[bits] + 1].min
   end
 end
 
-puts dp[-1][-1] == Float::INFINITY ? -1 : dp[-1][-1]
+puts dp[(1 << N) - 1] == Float::INFINITY ? -1 : dp[(1 << N) - 1]
