867f829e43e70423acd54cb1205d851d753871b4
fumtas1k <fumta01a7@gmail.com>
2023-09-18 00:35:04 +0900

    [change]優先度付キューのロジック変更

--- ruby/lib/priority_queue.rb
+++ ruby/lib/priority_queue.rb
@@ -1,94 +1,52 @@
 # 優先度付きキュー
-# 要素の配列の1番目を元にheapを構成する
 
 class PriorityQueue
+  private attr_accessor :heap, :ope
 
-  def initialize
-    @queue = [[Float::INFINITY]]
+  def initialize(arr = [], &block)
+    @heap = []
+    @ope = block || proc { |x, y| x <= y }
+    push_all(arr)
   end
 
-  def self.from(list)
-    instance = PriorityQueue.new
-    instance.push_all(list)
-    instance
+  def size = heap.size
+
+  def first = heap.first
+
+  def empty? = heap.empty?
+
+  def <<(x)
+    i = size
+    while i > 0
+      parent_idx = (i - 1) / 2
+      break if ope.call(heap[parent_idx], x)
+      heap[i] = heap[parent_idx]
+      i = parent_idx
+    end
+    heap[i] = x
   end
 
-  def queue
-    @queue[1..]
-  end
-
-  def size
-    @queue.size - 1
-  end
-
-  def push(arr)
-    @queue << arr
-    queue_up(size)
-  end
-
-  def push_all(list)
-    list.each { push(_1) }
+  def push_all(arr)
+    arr.each { self << _1 }
   end
 
   def shift
-    return if size.zero?
-    root = @queue[1]
-    data = @queue.pop
-    return root if size.zero?
-    @queue[1] = data
-    queue_down(1)
-    root
-  end
-
-  def first
-    return if size.zero?
-    @queue[1]
-  end
-
-  def to_s
-    queue.to_s
-  end
-
-  private
-
-  def parent_index(index)
-    index / 2
-  end
-
-  def left_child_index(index)
-    index * 2
-  end
-
-  def right_child_index(index)
-    index * 2 + 1
-  end
-
-  def min_child_index(index)
-    left, right = left_child_index(index), right_child_index(index)
-    return left if right > size
-    @queue[left].first <= @queue[right].first ? left : right
-  end
-
-  def swap(index1, index2)
-    @queue[index1], @queue[index2] = @queue[index2], @queue[index1]
-  end
-
-  def queue_up(index)
-    while (parent = parent_index(index)) > 0
-      if @queue[index].first < @queue[parent].first
-        swap(index, parent)
+    return if empty?
+    min = first
+    x = heap.pop
+    unless empty?
+      i = 0
+      while (child_idx1 = 2 * i + 1) < size
+        child_idx2 = child_idx1 + 1
+        if child_idx2 < size && !ope.call(heap[child_idx1], heap[child_idx2])
+          child_idx1 = child_idx2
+        end
+        break if ope.call(x, heap[child_idx1])
+        heap[i] = heap[child_idx1]
+        i = child_idx1
       end
-      index = parent
+      heap[i] = x
     end
-  end
-
-  def queue_down(index)
-    while (child = left_child_index(index)) <= size
-      min_child = min_child_index(index)
-      if @queue[index].first > @queue[min_child].first
-        swap(index, min_child)
-      end
-      index = min_child
-    end
+    min
   end
 end
--- ruby/spec/priority_queue_spec.rb
+++ ruby/spec/priority_queue_spec.rb
@@ -2,17 +2,17 @@ require_relative "../lib/priority_queue"
 
 # 優先度付キューのテスト
 RSpec.describe PriorityQueue do
-  let(:priority_queue) { PriorityQueue.new }
+  let(:priority_queue) { PriorityQueue.new { |x, y| x.first <= y.first } }
   context "8個の要素をプッシュした場合" do
     before do
-      priority_queue.push([9, 2])
-      priority_queue.push([5, 1])
-      priority_queue.push([3, 3])
-      priority_queue.push([6, 5])
-      priority_queue.push([8, 10])
-      priority_queue.push([1, 4])
-      priority_queue.push([4, 9])
-      priority_queue.push([2, 6])
+      priority_queue << [9, 2]
+      priority_queue << [5, 1]
+      priority_queue << [3, 3]
+      priority_queue << [6, 5]
+      priority_queue << [8, 10]
+      priority_queue << [1, 4]
+      priority_queue << [4, 9]
+      priority_queue << [2, 6]
     end
 
     it "sizeで要素数8が取得できる" do
@@ -49,34 +49,34 @@ RSpec.describe PriorityQueue do
       ])
     end
 
-    it "1つずつプッシュした場合と同じqueueが得られる" do
-      expected = PriorityQueue.new
-      expected.push([9, 2])
-      expected.push([5, 1])
-      expected.push([3, 3])
-      expected.push([6, 5])
-      expected.push([8, 10])
-      expected.push([1, 4])
-      expected.push([4, 9])
-      expected.push([2, 6])
-      expect(priority_queue.queue).to eq expected.queue
+    it "1つずつプッシュした場合と同じheapが得られる" do
+      expected = PriorityQueue.new { |x, y| x.first <= y.first }
+      expected << [9, 2]
+      expected << [5, 1]
+      expected << [3, 3]
+      expected << [6, 5]
+      expected << [8, 10]
+      expected << [1, 4]
+      expected << [4, 9]
+      expected << [2, 6]
+      expect(priority_queue.send(:heap)).to eq expected.send(:heap)
     end
   end
 
   context "配列で新規作成した場合" do
     before do
-      priority_queue.push([9, 2])
-      priority_queue.push([5, 1])
-      priority_queue.push([3, 3])
-      priority_queue.push([6, 5])
-      priority_queue.push([8, 10])
-      priority_queue.push([1, 4])
-      priority_queue.push([4, 9])
-      priority_queue.push([2, 6])
+      priority_queue << [9, 2]
+      priority_queue << [5, 1]
+      priority_queue << [3, 3]
+      priority_queue << [6, 5]
+      priority_queue << [8, 10]
+      priority_queue << [1, 4]
+      priority_queue << [4, 9]
+      priority_queue << [2, 6]
     end
 
-    it "1つずつプッシュした場合と同じqueueが得られる" do
-      actual = PriorityQueue.from([
+    it "1つずつプッシュした場合と同じheapが得られる" do
+      actual = PriorityQueue.new([
         [9, 2],
         [5, 1],
         [3, 3],
@@ -85,8 +85,20 @@ RSpec.describe PriorityQueue do
         [1, 4],
         [4, 9],
         [2, 6],
-      ])
-      expect(actual.queue).to eq priority_queue.queue
+      ]) { |x, y| x.first <= y.first }
+      expect(actual.send(:heap)).to eq priority_queue.send(:heap)
+    end
+  end
+
+  context "すべてを取り出した場合" do
+    before do
+      priority_queue << [9, 2]
+      priority_queue << [5, 1]
+      priority_queue.shift
+      priority_queue.shift
+    end
+    it "heapは空" do
+      expect(priority_queue.empty?).to be_truthy
     end
   end
 end
