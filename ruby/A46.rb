7cfb8e1c6f41825f8d3ab0c80981946ce3cb92d7
fumtas1k <fumta01a7@gmail.com>
2024-01-06 11:56:25 +0900

    [improve]

--- ruby/A46.rb
+++ ruby/A46.rb
@@ -3,7 +3,7 @@
 # Heuristic
 # 実行時間: 1s以内
 
-def measure_dist(u, v)
+def calc_dist(u, v)
   Math.sqrt(u.zip(v).sum { (_1[0] - _1[1]) ** 2 })
 end
 
@@ -14,11 +14,12 @@ end
 N = gets.to_i
 XY = Array.new(N) { gets.split.map(&:to_i) }
 
-visited = [true] + [false] * N
-dists = Array.new(N + 1) { [Float::INFINITY] * (N + 1) }
+visited = [false] * (N + 1)
+visited[0] = true
+dists = Array.new(N + 1) { [0] * (N + 1) }
 N.times do |i|
-  (i ... N).each do |j|
-    dists[i + 1][j + 1] = dists[j + 1][i + 1] = measure_dist(XY[i], XY[j])
+  (i + 1).upto(N - 1) do |j|
+    dists[i + 1][j + 1] = dists[j + 1][i + 1] = calc_dist(XY[i], XY[j])
   end
 end
 
