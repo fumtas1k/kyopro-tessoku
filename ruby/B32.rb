5cc0f181e408e2e1994aaf63cd6d2477db84384a
fumtas1k <fumta01a7@gmail.com>
2024-05-19 00:21:15 +0900

    [improve]シンプルに

--- ruby/B32.rb
+++ ruby/B32.rb
@@ -4,15 +4,12 @@
 # 実行時間(5s)
 
 N, K = gets.split.map(&:to_i)
-A = gets.split.map(&:to_i).sort
+A = gets.split.map(&:to_i)
 # dp[i: 石の数]　:= 先手が勝ち true, 負け false
-dp = [false] * (N + 1)
+dp = Array.new(N + 1, false)
 
 1.upto(N) do |i|
-  A.each do |j|
-    break if dp[i] || i < j
-    dp[i] = true unless dp[i - j]
-  end
+  dp[i] = A.any? { i >= _1 && !dp[i - _1] }
 end
 
 puts dp[-1] ? "First" : "Second"
