9cb33c36c113091b77e8de535c7cb020e5f50352
fumtas1k <fumta01a7@gmail.com>
2024-06-22 16:59:55 +0900

    [improve]変数名等修正

--- ruby/A49.rb
+++ ruby/A49.rb
@@ -3,53 +3,46 @@
 # Heuristic
 # 実行時間: 1s以内
 
-State = Struct.new(:score, :x, :last_move, :last_pos) {
-  def <=>(other)
-    score <=> other.score
-  end
+State = Struct.new(:score, :arr, :move, :pre) {
+  def <=>(other) = score <=> other.score
 }
 
 WIDTH = 2_000
 N = 20
 T = gets.to_i
-PQR = Array.new(T) { gets.split.map(&:to_i) }
+PQR = Array.new(T) { gets.split.map(&:to_i).map(&:pred) }
+beams = []
+beams << [State.new(0, Array.new(N, 0), nil, -1)]
 
-num_state = []
-beam = []
-
-num_state[0] = 1
-beam << [State.new(0, Array.new(N + 1, 0), nil, -1)]
 PQR.each_with_index do |pqr, i|
-  candidate = []
-  num_state[i].times do |j|
-    state_a = beam[i][j].clone
-    state_a.x = beam[i][j].x.clone
-    state_a.last_move = "A"
-    state_a.last_pos = j
-    pqr.each { state_a.x[_1] += 1 }
-    state_a.score += state_a.x.count(0)
-    candidate << state_a
+  candidates = []
+  beams[i].each_with_index do |state, j|
+    sousa_a = state.dup
+    sousa_a.arr = state.arr.dup
+    sousa_a.move = :A
+    sousa_a.pre = j
+    pqr.each { sousa_a.arr[_1] += 1 }
+    sousa_a.score = sousa_a.arr.count(0)
+    candidates << sousa_a
 
-    state_b = beam[i][j].clone
-    state_b.x = beam[i][j].x.clone
-    state_b.last_move = "B"
-    state_b.last_pos = j
-    pqr.each { state_b.x[_1] -= 1 }
-    state_b.score += state_b.x.count(0)
-    candidate << state_b
+    sousa_b = state.dup
+    sousa_b.arr = state.arr.dup
+    sousa_b.move = :B
+    sousa_b.pre = j
+    pqr.each { sousa_b.arr[_1] -= 1 }
+    sousa_b.score = sousa_b.arr.count(0)
+    candidates << sousa_b
   end
-
-  candidate.sort!.reverse!
-
-  num_state << [WIDTH, candidate.size].min
-  beam << candidate[0, num_state[-1]]
+  candidates.sort!.reverse!
+  size = [WIDTH, candidates.size].min
+  beams << candidates[0, size]
 end
 
 ans = []
 pos = 0
 T.downto(1) do |i|
-  ans[i - 1] = beam[i][pos].last_move
-  pos = beam[i][pos].last_pos
+  ans << beams[i][pos].move
+  pos = beams[i][pos].pre
 end
 
-puts ans
+puts ans.reverse
