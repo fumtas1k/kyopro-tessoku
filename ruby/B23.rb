61d2052163cf04972e2f68ba7dc2b9fd65abee0d
fumtas1k <fumta01a7@gmail.com>
2024-08-31 02:00:06 +0900

    [add]説明追加

--- ruby/B23.rb
+++ ruby/B23.rb
@@ -5,24 +5,26 @@
 
 N = gets.to_i
 XY = Array.new(N) { gets.split.map(&:to_i) }
-DIST = Array.new(N) { Array.new(N, 0) }
+dist = Array.new(N) { Array.new(N, 0) }
 N.times do |i|
   (i + 1).upto(N - 1) do |j|
-    DIST[i][j] = DIST[j][i] = XY[i].zip(XY[j]).sum { (_1 - _2) ** 2 }.then { Math.sqrt(_1) }
+    dist[i][j] = dist[j][i] = XY[i].zip(XY[j]).sum { (_1 - _2) ** 2 }.then { Math.sqrt(_1) }
   end
 end
 
-dp = Array.new(1 << N) { Array.new(N, Float::INFINITY) }
+# dp[i][bit]: bitの集合に含まれる頂点を全て通って頂点iに到達する最短距離
+dp = Array.new(N) { Array.new(1 << N, Float::INFINITY) }
+# 循環するため出発地点を0に固定しても一般性は失われない
 dp[0][0] = 0
 
 (1 << N).times do |bit|
   N.times do |i|
-    next if dp[bit][i] == Float::INFINITY
+    next if dp[i][bit] == Float::INFINITY
     N.times do |j|
       next if bit[j] == 1 || i == j
-      dp[bit | (1 << j)][j] = [dp[bit | (1 << j)][j], dp[bit][i] + DIST[i][j]].min
+      dp[j][bit | (1 << j)] = [dp[j][bit | (1 << j)], dp[i][bit] + dist[i][j]].min
     end
   end
 end
 
-puts dp[-1][0]
+puts dp[0][-1]
