29e47be866076d60b19256e3e2ad224bb70c9e24
fumtas1k <fumta01a7@gmail.com>
2024-09-01 11:42:26 +0900

    [improve]シンプルに

--- ruby/B24.rb
+++ ruby/B24.rb
@@ -7,14 +7,12 @@
 N = gets.to_i
 # xの最小順にし、yが同じ場合はyの最大順になるよう並び替えることで
 # yの最長増加部分列として問題を解くことができる
-XY = Array.new(N) { gets.split.map(&:to_i) }.sort_by { [_1[0], -_1[1]] }
+XY = Array.new(N) { gets.split.map(&:to_i) }.sort_by { [_1, -_2] }
 
 lis = []
-
-XY.each_with_index do |(_, y), i|
-  lis << i if lis.empty?
-  idx = lis.bsearch_index { XY[_1][1] >= y } || lis.size
-  lis[idx] = i
+XY.each do |x, y|
+  idx = lis.bsearch_index { _2 >= y } || lis.size
+  lis[idx] = [x, y]
 end
 
 puts lis.size
