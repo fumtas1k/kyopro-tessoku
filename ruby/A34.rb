7d27aec55825d639a61eb8f8aa895b98b5c0c19c
fumtas1k <fumta01a7@gmail.com>
2025-10-13 11:25:15 +0900

    グランディ数の計算ロジックを改善し、遷移処理を簡素化しました

--- ruby/A34.rb
+++ ruby/A34.rb
@@ -2,22 +2,17 @@
 # grundy数
 # 実行時間: 1s以内
 
+MAX = 100_000
 N, X, Y = gets.split.map(&:to_i)
 A = gets.split.map(&:to_i)
-MAX = A.max
-grundy = [0] * (MAX + 1)
 
-1.upto(MAX) do |i|
+grundy = [0] * (MAX + 1)
+X.upto(MAX) do |i|
   # 遷移は2パターンしかないので、grundy数は[0, 1, 2]の3パターンしかない
-  transit = [false] * 3
-  transit[grundy[i - X]] = true if i >= X
-  transit[grundy[i - Y]] = true if i >= Y
-  3.times do |j|
-    unless transit[j]
-      grundy[i] = j
-      break
-    end
-  end
+  candidates = [0, 1, 2]
+  candidates.delete(grundy[i - X])
+  candidates.delete(grundy[i - Y]) if i >= Y
+  grundy[i] = candidates[0]
 end
 
 puts A.map { grundy[_1] }.reduce(:^).zero? ? "Second" : "First"
